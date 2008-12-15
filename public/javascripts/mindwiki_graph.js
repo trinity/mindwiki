9e60342a53c7b9ef28a70d4e387ce2cd3d1839a0
mika.hannula <mika.hannula@2932d2ce-a958-0410-ba3e-8fac7ae6c6e8>
2008-12-15 21:12:09 +0000

    Mika: Here are my scripts. They need jquery.livequery.js to work properly. I was not able to add it to the project :(
    git-svn-id: https://mwsvn.piipari.net/mindwiki/trunk@38 2932d2ce-a958-0410-ba3e-8fac7ae6c6e8

--- public/javascripts/mindwiki_graph.js
+++ public/javascripts/mindwiki_graph.js
@@ -1,9 +1,13 @@
+
 // This file defines the MindWiki graph viewing and editing client
 
 // Global baaad?
 var graph_id;
 // TEMP! We should implement an actual data model for the graph.
 var notes = new Array();
+
+// tmp. test. delete asap...
+var currentId = 1000;
 
 window.onload = function(){
 
@@ -13,9 +17,125 @@ window.onload = function(){
   graph_id = parseInt(id_from_pathname[0]); // RegExp.exec puts matches into an array
 
   // Note creation by double clicking in the window
-  $("div.mindwiki_window").dblclick(function () {
+  /*
+  $("#vport").dblclick(function () {
     createNote();
   });
+  */
+	$("#vport").dblclick( function(event)
+	{
+		// it's probably a good idea to put all these to createNote function...
+		var newNote = document.createElement("div");
+		var newContent = document.createElement("table");
+		var newTitle = document.createElement("td");
+		var newArticleTD = document.createElement("td");
+		var row0 = document.createElement("tr");
+		var row1 = document.createElement("tr");
+		var row2 = document.createElement("tr");
+		var newArticle = document.createElement("div");
+		var deleteButtonTD = document.createElement("td");
+		var colorButtonTD = document.createElement("td");
+		var arrowButtonTD = document.createElement("td");
+		var deleteButton = document.createElement("a");
+		var colorButton = document.createElement("a");
+		var arrowButton = document.createElement("a");
+			
+		// this is used until we get a real id from the database...
+		currentId++;
+			
+		var offset = $(this).offset();
+			
+		// buttons
+		// note: tag attribute is used for getting the note id. there must be a better way
+		// to do this (like parents in jQuery), but this was the first solution that worked.
+		$(deleteButton).addClass("deleteButton").attr("id", "del"+currentId.toString()).attr("tag", currentId.toString())
+		.attr("href", "#").append("Delete");
+			
+		$(deleteButtonTD).addClass("noteButtonTD").append(deleteButton);
+			
+		// rows
+		$(row0).append(deleteButtonTD).attr("id", "row0_"+currentId.toString());
+			
+		$(newTitle).addClass("noteTitle").attr("id", "title"+currentId.toString())
+		.append("Lappu "+currentId.toString());
+			
+		$(newArticle).addClass("noteArticle").attr("id", "article"+currentId.toString())
+		.append("Sisältöä...");
+			
+		$(newArticleTD).addClass("noteArticleTD").attr("id", "articleTD"+currentId.toString())
+		.append(newArticle);
+			
+		$(row1).append(newTitle).attr("id", "row1_"+currentId.toString());
+		$(row2).append(newArticleTD).attr("id", "row2_"+currentId.toString());
+
+		$(newContent).addClass("noteContent").attr("id", "content"+currentId.toString())
+		.append(row0).append(row1).append(row2);
+			
+		$(newNote).addClass("note").attr("id", currentId.toString()).resizable(
+		{
+			minWidth: 100,
+			minHeight: 60,
+			maxWidth: 400,
+			maxHeight: 400,
+			handles:  'se',
+				
+		})
+		.draggable(
+		{
+			zIndex:	currentId,
+			containment : "parent"
+		})
+		.css("top", function ()
+		{
+			return event.pageY - offset.top;
+		})
+		.css("left", function ()
+		{
+			return event.pageX - offset.left;
+		})
+		.append(newContent).appendTo(this);
+	}
+	);
+		
+	$(".note").livequery("click", function(event)
+	{
+		// select:
+		// - unselect previously selected if any
+		// - show buttons
+		// - highlight borders?
+			
+		$(this).css("border", "4px solid red");
+		event.stopPropagation();
+		}
+	);
+		
+	$(".noteArticle").livequery("dblclick", function(event)
+	{
+		// launch edit...
+		this.style.backgroundColor = "#aa7777";
+		event.stopPropagation();
+	}
+	);
+		
+	$(".noteTitle").livequery("dblclick", function(event)
+	{
+		// this event is not used. we just prevent the dblclick
+		// to bubble to parents.
+		event.stopPropagation();
+	}
+	);
+		
+	$(".deleteButton").livequery("click", function(event)
+	{
+		// this event is not used. we just prevent the dblclick
+		// to bubble to parents.
+		$(this).append(" - clicked");
+		var deleteNote = document.getElementById($(this).attr("tag"));
+		document.getElementById("vport").removeChild(deleteNote);
+		delete deleteNote;
+		event.stopPropagation();
+	}
+	);
 
   loadAllNotes();
 
@@ -56,7 +176,7 @@ function loadNote(id, name,x,y,width,height,color,content) {
   $(newNote).resizable().draggable({containment: "parent"});
   // Adding to all mindwiki_windows-classes. Maybe a bit over the top, as an ID would be fine.
   notes[id] = newNote;
-  $("div.mindwiki_window").append(newNote);
+  $("#vport").append(newNote);
 };
 // MAYBE MAKE A FUNCTION TO DRAW THE EDGES?!
 // Some other day...
@@ -113,7 +233,7 @@ function createNote() {
   $(newNote).append("<p><table><td>Edit note</td><td> Delete note</td></table></p>");
   $(newNote).resizable().draggable({containment: "parent"});
   // Adding to all mindwiki_windowses within the class. Maybe a bit over the top, as an ID would be fine.
-  $("div.mindwiki_window").append(newNote);
+  $("#vport").append(newNote);
 };
 
 // TEST: Does this now call asdf-function every 5 secs?
