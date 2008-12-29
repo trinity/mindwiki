e8418b3025076fbd6a738c59398ce261d86230dc
jukka.peltomaki <jukka.peltomaki@2932d2ce-a958-0410-ba3e-8fac7ae6c6e8>
2008-12-29 15:56:28 +0000

    Validation and testing for Notes-model.
    git-svn-id: https://mwsvn.piipari.net/mindwiki/trunk@46 2932d2ce-a958-0410-ba3e-8fac7ae6c6e8

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,21 @@
 class Note < ActiveRecord::Base
+
   belongs_to :article
   belongs_to :graph
+  validates_presence_of :graph
   
   has_many :edges_to, :foreign_key => "source_id", :class_name => "Edge"
   has_many :edges_from, :foreign_key => "target_id", :class_name => "Edge"
+  
+
+  validates_presence_of :name
+  # No checking of sane x/y values, cause after viewport integration these
+  # virtual coordinates might even be negative.
+  validates_numericality_of :x, :only_integer => true
+  validates_numericality_of :y, :only_integer => true
+  validates_numericality_of :width, :greater_than_or_equal_to => 20, :less_than => 9999, :only_integer => true
+  validates_numericality_of :height, :greater_than_or_equal_to => 20, :less_than => 9999, :only_integer => true
+  # W3C HTML & CSS standards only accept 16 color names
+  validates_format_of :color, :with => /\A(#(\d|a|b|c|d|e|f){6,6})|(aqua)|(black)|(blue)|(fuchsia)|(gray)|(green)|(lime)|(maroon)|(navy)|(olive)|(purple)|(red)|(silver)|(teal)|(white)|(yellow)\Z/i
                                                      
 end
--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -1,8 +1,99 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :notes, :graphs
+
+  # Simple creation
+  test "Simple note creation." do
+    assert_difference 'Note.count' do
+      note = Note.new({
+        :name => "",
+        :x => 10,
+        :y => 10,
+        :width => 300,
+        :height => 200,
+        :color => "#abba10"
+      })
+
+      # Blank name shouldn't be allowed
+      assert_equal false, note.save
+      note.name = "New note for testing"
+      
+      # Reference to a graph needed!
+      assert_equal false, note.save
+      graph = Graph.find(graphs(:ruby_graph).id)
+      note.graph = graph
+
+      # No need for article-reference at this time. Should there be? Maybe...
+
+      # Now it should save
+      assert note.save
+    end
   end
+
+  # Position acceptance
+  test "Position changing test" do
+    note = Note.find(notes(:ruby_note).id)
+    
+    # Currently even negative integers are okay
+    assert note.update_attributes(:x => 0)
+    assert note.update_attributes(:y => 0)
+    assert note.update_attributes(:x => 100)
+    assert note.update_attributes(:y => 100)
+    assert note.update_attributes(:x => -9999)
+    assert note.update_attributes(:y => -9999)
+    
+    # Only integers, though
+    assert_equal false, note.update_attributes(:x => 4.5)
+    assert_equal false, note.update_attributes(:y => 4.5)
+    assert_equal false, note.update_attributes(:x => "foo")
+    assert_equal false, note.update_attributes(:y => "foo")
+    assert_equal false, note.update_attributes(:x => nil)
+    assert_equal false, note.update_attributes(:y => nil)
+  end
+
+  # Size
+  test "Size changing test" do
+    note = Note.find(notes(:ruby_note).id)
+    
+    assert note.update_attributes(:width => 300)
+    assert note.update_attributes(:height => 300)
+    assert note.update_attributes(:width => 1000)
+    assert note.update_attributes(:height => 500)
+    assert note.update_attributes(:width => 9998)
+    assert note.update_attributes(:height => 9998)
+    
+    assert_equal false, note.update_attributes(:width => 4.5)
+    assert_equal false, note.update_attributes(:height => 4.5)
+    assert_equal false, note.update_attributes(:width => "foo")
+    assert_equal false, note.update_attributes(:height => "foo")
+    assert_equal false, note.update_attributes(:width => nil)
+    assert_equal false, note.update_attributes(:height => nil)
+
+    # Too small
+    assert_equal false, note.update_attributes(:width => 10)
+    assert_equal false, note.update_attributes(:height => 10)
+
+    # Too big
+    assert_equal false, note.update_attributes(:width => 9999)
+    assert_equal false, note.update_attributes(:height => 9999)
+
+    # No negatives
+    assert_equal false, note.update_attributes(:width => -300)
+    assert_equal false, note.update_attributes(:height => -300)
+  end
+  
+  # Color acceptance
+  test "Color changing test" do
+    note = Note.find(notes(:ruby_note).id)
+
+    # Normal six-figure hexes should work fine, three-figures shouldn't
+    assert note.update_attributes(:color => "#b1b1b1")
+    assert_equal false, note.update_attributes(:color => "#ccc")
+
+    # 16 W3C standard colors should work, e.g. yellow works, pink doesn't
+    assert note.update_attributes(:color => "yellow")
+    assert_equal false, note.update_attributes(:color => "pink")
+  end
+  
 end
