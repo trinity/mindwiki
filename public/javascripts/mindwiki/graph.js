3874e9886079ec65038b73770f6a0ce76f9705e9
samu.ollila <samu.ollila@2932d2ce-a958-0410-ba3e-8fac7ae6c6e8>
2009-02-14 17:37:24 +0000

    A small change to the colorpicker.
    git-svn-id: https://mwsvn.piipari.net/mindwiki/trunk@176 2932d2ce-a958-0410-ba3e-8fac7ae6c6e8

--- public/javascripts/mindwiki/graph.js
+++ public/javascripts/mindwiki/graph.js
@@ -261,6 +261,11 @@ function Graph() {
       $(picker).fadeOut(100);
       return false;
     },
+    onChange: function (hsb, hex, rgb) {
+	  graph.selectedNote.color = "#"+hex;
+      graph.selectedNote.update();
+      thisgraph.sync.setNoteColor(thisgraph.selectedNote.id, "#"+hex);
+	},
     onSubmit: function(hsb, hex, rgb){
       graph.selectedNote.color = "#"+hex;
       graph.selectedNote.update();
