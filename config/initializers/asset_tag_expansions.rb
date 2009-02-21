12f645894e3ce0374d29ecf64db7b10ee4af7ca1
aapo.tahkola <aapo.tahkola@2932d2ce-a958-0410-ba3e-8fac7ae6c6e8>
2009-02-21 10:05:25 +0000

    Add jquery-ui theme picked from http://jqueryui.com/themeroller/. It is easier to use jquery-ui built in class names than to specify our own with addClass.
    git-svn-id: https://mwsvn.piipari.net/mindwiki/trunk@193 2932d2ce-a958-0410-ba3e-8fac7ae6c6e8

--- config/initializers/asset_tag_expansions.rb
+++ config/initializers/asset_tag_expansions.rb
@@ -2,16 +2,19 @@
 
 # Javascript-files required by the MindWiki application (not all of the site, just the application)
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :mindwiki_graph => [
- "jquery-1.3.2", "jquery-ui-personalized-1.6rc6", "jquery.livequery", "jquery.url", "jquery.scrollTo", "jquery.jeditable",
+ "jquery-1.3.2", "jquery-ui-personalized-1.6rc6",
+ "jquery.livequery", "jquery.url", "jquery.scrollTo", "jquery.jeditable",
  "raphael", "colorpicker",
  "markitup/jquery.markitup", "markitup/sets/textile/set",
  "mindwiki/misc", "mindwiki/context_help", "mindwiki/sync", "mindwiki/note", 
  "mindwiki/edge", "mindwiki/graph", "mindwiki/viewport", "mindwiki/config"]
 
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :graph_color_picker => ["jquery-1.2.6.min", "colorpicker", "graph_color_init"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :graph_color_picker => ["jquery-1.3.2", "colorpicker", "graph_color_init"]
  
 # Stylesheet
-ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :mindwiki_graph => ["/javascripts/markitup/skins/markitup/style",
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :mindwiki_graph => [
+ "ui.all",
+ "/javascripts/markitup/skins/markitup/style",
  "/javascripts/markitup/sets/textile/style", "mindwiki_graph", "colorpicker"]
 
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :graph_color_picker => ["colorpicker"]
