7deee3712279293990bc10a13fd6e4d89b18333a
aapo.tahkola <aapo.tahkola@2932d2ce-a958-0410-ba3e-8fac7ae6c6e8>
2009-02-23 11:05:44 +0000

    Missed asset_tag_expansions.rb
    git-svn-id: https://mwsvn.piipari.net/mindwiki/trunk@199 2932d2ce-a958-0410-ba3e-8fac7ae6c6e8

--- config/initializers/asset_tag_expansions.rb
+++ config/initializers/asset_tag_expansions.rb
@@ -3,7 +3,7 @@
 # Javascript-files required by the MindWiki application (not all of the site, just the application)
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :mindwiki_graph => [
  "jquery-1.3.2", "jquery-ui-personalized-1.6rc6",
- "jquery.livequery", "jquery.url", "jquery.scrollTo", "jquery.jeditable",
+ "jquery.livequery", "jquery.url", "jquery.scrollTo", "jquery.jeditable", "jquery.mousewheel",
  "raphael", "colorpicker",
  "markitup/jquery.markitup", "markitup/sets/textile/set",
  "mindwiki/misc", "mindwiki/context_help", "mindwiki/sync", "mindwiki/note", 
