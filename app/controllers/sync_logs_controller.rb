83211a94c25be6835b80c04497aa9d26d86fdefe
jukka.peltomaki <jukka.peltomaki@2932d2ce-a958-0410-ba3e-8fac7ae6c6e8>
2009-03-20 07:27:53 +0000

    Temporary debugging for check_for_updates.
    git-svn-id: https://mwsvn.piipari.net/mindwiki/trunk@250 2932d2ce-a958-0410-ba3e-8fac7ae6c6e8

--- app/controllers/sync_logs_controller.rb
+++ app/controllers/sync_logs_controller.rb
@@ -14,6 +14,7 @@ class SyncLogsController < ApplicationController
         if !params[:timestamp].empty?
           # graph_id = NULL means we push the update to all clients regardless of active graph
           @updates = SyncLog.find(:all, :order => "created_at ASC", :conditions => [ "created_at >= ? AND created_at < ? AND (graph_id = ? OR graph_id IS NULL)", params[:timestamp], @now, params[:id]])
+          @debug = { :timestamp => params[:timestamp], :now => @now, :gid => params[:id] }
           @graph = Graph.find(:first, :conditions => ["id = ?", params[:id]]) # find(params[:id]) raises a RecordNotFound error upon deleted graph
           if !@graph.nil? and @updates.count > 0
             @exts = @graph.get_extents
@@ -21,11 +22,12 @@ class SyncLogsController < ApplicationController
         else
           @exts = ""
           @updates = ""
+          @debug = ""
           # With an OK timestamp but no updates, the response is "exts: null and updates: []"
         end
         format.js { 
           # Give the client the timestamp we used as an upper bound, and also the results
-          @updates_and_timestamp = "{\"time\": "+@now.to_json+", \"extents\": "+@exts.to_json+", \"updates\": "+@updates.to_json+"}"
+          @updates_and_timestamp = "{\"time\": "+@now.to_json+", \"extents\": "+@exts.to_json+", \"updates\": "+@updates.to_json+", \"debug\": "+@debug.to_json+"}"
           render :json => @updates_and_timestamp 
         }
       end
